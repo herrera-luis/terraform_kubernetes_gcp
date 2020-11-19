8b1c1ee22e27eb5dbccc76c61529d00da6d00fd9
Luis Herrera <herrera_luis10@hotmail.com>
2020-11-19 11:09:43 -0500

    feat: adds backend config to save status on GCP bucket

--- versions.tf
+++ versions.tf
@@ -1,3 +1,8 @@
 terraform {
   required_version = ">= 0.13"
+  backend "gcs" {
+    bucket          = "devops-lab-tf-state" // you have to create this bucket on GCP before run terraform init command
+    prefix          = "/terraform.tfstate"
+    credentials     = "/opt/devops-lab.json"
+  }
 }
