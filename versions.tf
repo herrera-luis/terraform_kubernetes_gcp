6e6309b2cf2923fefb40ecbfe6d013a9a0ac4086
Luis Herrera <herrera_luis10@hotmail.com>
2020-11-19 11:08:51 -0500

    refactor: updates versions to save terraform backend status locally

--- versions.tf
+++ versions.tf
@@ -1,8 +1,3 @@
 terraform {
   required_version = ">= 0.13"
-  backend "gcs" {
-    bucket          = "devops-lab-tf-state" // you have to create this bucket on GCP before run terraform init command
-    prefix          = "/terraform.tfstate"
-    credentials     = "/opt/devops-lab.json"
-  }
 }
